d4f18af8ad90e56a6a47819037d581547ff67ce0
Helium <1029535012@qq.com>
2017-11-07 17:28:38 +0800

    DirectGraphics DLL

---
+++ DirectGraphics/DirectGraphics.cpp
@@ -0,0 +1,418 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017, Alopex/Helium
+*     All rights reserved.
+*
+* @file		DirectGraphics.cpp
+* @brief	This Program is DirectGraphics DLL Project.
+* @author	Alopex/Helium
+* @version	v1.00a
+* @date		2017-11-2
+*/
+#include "DirectCommon.h"
+#include "DirectGraphics.h"
+
+//------------------------------------------------------------------
+// @Function:	 DirectGraphics()
+// @Purpose: DirectGraphics构造函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+DirectGraphics::DirectGraphics()
+{
+	m_pD3D9 = NULL;	//IDirect3D9接口指针初始化(NULL)
+	m_pD3D9Device = NULL;	//IDirect3DDevice9接口指针初始化(NULL)
+	ZeroMemory(&m_D3D9Caps, sizeof(m_D3D9Caps));	//清空m_D3D9Caps内存区域
+	ZeroMemory(&m_D3D9pp, sizeof(m_D3D9pp));	//清空m_D3D9pp内存区域
+	ZeroMemory(m_wcD3D9AdapterType, sizeof(wchar_t)*ADAPTERTYPESIZE);	//清空m_wcD3D9AdapterType内存区域
+}
+
+//------------------------------------------------------------------
+// @Function:	 ~DirectGraphics()
+// @Purpose: DirectGraphics析构函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+DirectGraphics::~DirectGraphics()
+{
+	SAFE_RELEASE(m_pD3D9Device);	//IDirect3D9接口指针释放
+	SAFE_RELEASE(m_pD3D9);	//IDirect3DDevice9接口指针释放
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectGraphicsGetObject(void) const
+// @Purpose: DirectGraphics读取D3D9对象
+// @Since: v1.00a
+// @Para: None
+// @Return: IDirect3D9*(D3D9对象指针)
+//------------------------------------------------------------------
+IDirect3D9* WINAPI DirectGraphics::DirectGraphicsGetObject(void) const
+{
+	return m_pD3D9;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectGraphicsGetDevice(void) const
+// @Purpose: DirectGraphics读取D3D9设备对象
+// @Since: v1.00a
+// @Para: None
+// @Return: IDirect3DDevice9*(D3D9设备对象指针)
+//------------------------------------------------------------------
+IDirect3DDevice9* WINAPI DirectGraphics::DirectGraphicsGetDevice(void) const
+{
+	return m_pD3D9Device;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectGraphicsGetCaps(void) const
+// @Purpose: DirectGraphics读取D3D9设备型号
+// @Since: v1.00a
+// @Para: None
+// @Return: D3DCAPS9*(D3D9设备型号指针)
+//------------------------------------------------------------------
+const D3DCAPS9* WINAPI DirectGraphics::DirectGraphicsGetCaps(void) const
+{
+	return (&m_D3D9Caps);
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectGraphicsGetPresentParameters(void) const
+// @Purpose: DirectGraphics读取D3D9设备参数
+// @Since: v1.00a
+// @Para: None
+// @Return: D3DPRESENT_PARAMETERS*(D3D9设备参数指针)
+//------------------------------------------------------------------
+const D3DPRESENT_PARAMETERS* WINAPI DirectGraphics::DirectGraphicsGetPresentParameters(void) const
+{
+	return (&m_D3D9pp);
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectGraphicsAdapterType(void) const
+// @Purpose: DirectGraphics读取D3D9 GPU型号
+// @Since: v1.00a
+// @Para: None
+// @Return: wchar_t*(宽字符数组地址)
+//------------------------------------------------------------------
+const wchar_t* WINAPI DirectGraphics::DirectGraphicsAdapterType(void) const
+{
+	return m_wcD3D9AdapterType;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectGraphicsInit(HWND hWnd)
+// @Purpose: DirectGraphics 初始化
+// @Since: v1.00a
+// @Para: HWND hWnd(窗口句柄)
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsInit(HWND hWnd)
+{
+	int nSize;
+	int Vp;
+	D3DADAPTER_IDENTIFIER9 D3D9Adapter;
+
+	//读取Direct3D9接口对象指针
+	m_pD3D9 = Direct3DCreate9(D3D_SDK_VERSION);//创建Direct3D9接口对象
+
+	//读取D3DCAPS9校验硬件顶点运算
+	VERIFY(m_pD3D9->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &m_D3D9Caps));//校验硬件顶点运算能力
+
+	if (m_D3D9Caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT)
+	{
+		Vp = D3DCREATE_HARDWARE_VERTEXPROCESSING;//支持硬件顶点运算(硬件顶点运算)
+	}
+	else
+	{
+		Vp = D3DCREATE_SOFTWARE_VERTEXPROCESSING;//不支持硬件顶点运算(软件顶点运算)
+	}
+
+	//填充D3DPRESENT_PARAMETERS结构
+	m_D3D9pp.BackBufferWidth = USER_SCREENWIDTH;//后台缓冲表面宽度(Pixel)
+	m_D3D9pp.BackBufferHeight = USER_SCREENHEIGHT;//后台缓冲表面高度(Pixel)
+	m_D3D9pp.BackBufferFormat = D3DFMT_A8R8G8B8;//后台缓冲像素格式
+	m_D3D9pp.BackBufferCount = 1;//后台缓冲数量(1)
+	m_D3D9pp.MultiSampleType = D3DMULTISAMPLE_NONE;//后台缓冲多重采样类型
+	m_D3D9pp.MultiSampleQuality = 0;//后台缓冲多重采样质量
+	m_D3D9pp.SwapEffect = D3DSWAPEFFECT_DISCARD;//交换链页面置换方式
+	m_D3D9pp.hDeviceWindow = hWnd;//设备相关窗口句柄
+	m_D3D9pp.Windowed = true;//窗口模式:true/全屏模式:false
+	m_D3D9pp.EnableAutoDepthStencil = true;//Direct3D自动创建维护深度缓冲和模板缓冲
+	m_D3D9pp.AutoDepthStencilFormat = D3DFMT_D24S8;//深度缓冲和模板缓冲像素格式
+	m_D3D9pp.Flags = 0;//无标记格式
+	m_D3D9pp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;//Direct3D默认刷新频率
+	m_D3D9pp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;//Direct3D提交频率(默认提交)
+
+	//创建IDirect3DDevice9接口对象指针
+	VERIFY(m_pD3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, Vp, &m_D3D9pp, &m_pD3D9Device));//创建IDirect3DDevice9接口对象指针
+
+	//读取GPU型号
+	m_pD3D9->GetAdapterIdentifier(0, 0, &D3D9Adapter);	//读取GPU信息
+	nSize = MultiByteToWideChar(CP_ACP, 0, D3D9Adapter.Description, -1, NULL, 0);
+	MultiByteToWideChar(CP_ACP, 0, D3D9Adapter.Description, -1, m_wcD3D9AdapterType, nSize);
+
+	return S_OK;//OK
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectGraphicsInit(HWND hWnd, bool bIsWindowed)
+// @Purpose: DirectGraphics 初始化
+// @Since: v1.00a
+// @Para: HWND hWnd(窗口句柄)
+// @Para: bool bIsWindowed(是否以窗口模式运行)
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsInit(HWND hWnd, bool bIsWindowed)
+{
+	int nSize;
+	int Vp;
+	D3DADAPTER_IDENTIFIER9 D3D9Adapter;
+
+	//读取Direct3D9接口对象指针
+	m_pD3D9 = Direct3DCreate9(D3D_SDK_VERSION);//创建Direct3D9接口对象
+
+	//读取D3DCAPS9校验硬件顶点运算
+	VERIFY(m_pD3D9->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &m_D3D9Caps));//校验硬件顶点运算能力
+
+	if (m_D3D9Caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT)
+	{
+		Vp = D3DCREATE_HARDWARE_VERTEXPROCESSING;//支持硬件顶点运算(硬件顶点运算)
+	}
+	else
+	{
+		Vp = D3DCREATE_SOFTWARE_VERTEXPROCESSING;//不支持硬件顶点运算(软件顶点运算)
+	}
+
+	//填充D3DPRESENT_PARAMETERS结构
+	m_D3D9pp.BackBufferWidth = USER_SCREENWIDTH;//后台缓冲表面宽度(Pixel)
+	m_D3D9pp.BackBufferHeight = USER_SCREENHEIGHT;//后台缓冲表面高度(Pixel)
+	m_D3D9pp.BackBufferFormat = D3DFMT_A8R8G8B8;//后台缓冲像素格式
+	m_D3D9pp.BackBufferCount = 1;//后台缓冲数量(1)
+	m_D3D9pp.MultiSampleType = D3DMULTISAMPLE_NONE;//后台缓冲多重采样类型
+	m_D3D9pp.MultiSampleQuality = 0;//后台缓冲多重采样质量
+	m_D3D9pp.SwapEffect = D3DSWAPEFFECT_DISCARD;//交换链页面置换方式
+	m_D3D9pp.hDeviceWindow = hWnd;//设备相关窗口句柄
+	m_D3D9pp.Windowed = bIsWindowed;//窗口模式:true/全屏模式:false
+	m_D3D9pp.EnableAutoDepthStencil = true;//Direct3D自动创建维护深度缓冲和模板缓冲
+	m_D3D9pp.AutoDepthStencilFormat = D3DFMT_D24S8;//深度缓冲和模板缓冲像素格式
+	m_D3D9pp.Flags = 0;//无标记格式
+	m_D3D9pp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;//Direct3D默认刷新频率
+	m_D3D9pp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;//Direct3D提交频率(默认提交)
+
+	//创建IDirect3DDevice9接口对象指针
+	VERIFY(m_pD3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, Vp, &m_D3D9pp, &m_pD3D9Device));//创建IDirect3DDevice9接口对象指针
+
+	//读取GPU型号
+	m_pD3D9->GetAdapterIdentifier(0, 0, &D3D9Adapter);	//读取GPU信息
+	nSize = MultiByteToWideChar(CP_ACP, 0, D3D9Adapter.Description, -1, NULL, 0);
+	MultiByteToWideChar(CP_ACP, 0, D3D9Adapter.Description, -1, m_wcD3D9AdapterType, nSize);
+
+	return S_OK;//OK
+}
+
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsInit(HWND hWnd, bool bIsWindowed, int nScreenWidth, int nScreenHeight)
+// @Purpose: DirectGraphics 初始化
+// @Since: v1.00a
+// @Para: HWND hWnd(窗口句柄)
+// @Para: bool bIsWindowed(是否以窗口模式运行)
+// @Para: int nScreenWidth(屏幕宽度)
+// @Para: int nScreenHeight(屏幕高度)
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsInit(HWND hWnd, bool bIsWindowed, int nScreenWidth, int nScreenHeight)
+{
+	int nSize;
+	int Vp;
+	D3DADAPTER_IDENTIFIER9 D3D9Adapter;
+
+	//读取Direct3D9接口对象指针
+	m_pD3D9 = Direct3DCreate9(D3D_SDK_VERSION);//创建Direct3D9接口对象
+
+	//读取D3DCAPS9校验硬件顶点运算
+	VERIFY(m_pD3D9->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &m_D3D9Caps));//校验硬件顶点运算能力
+
+	if (m_D3D9Caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT)
+	{
+		Vp = D3DCREATE_HARDWARE_VERTEXPROCESSING;//支持硬件顶点运算(硬件顶点运算)
+	}
+	else
+	{
+		Vp = D3DCREATE_SOFTWARE_VERTEXPROCESSING;//不支持硬件顶点运算(软件顶点运算)
+	}
+
+	//填充D3DPRESENT_PARAMETERS结构
+	m_D3D9pp.BackBufferWidth = nScreenWidth;//后台缓冲表面宽度(Pixel)
+	m_D3D9pp.BackBufferHeight = nScreenHeight;//后台缓冲表面高度(Pixel)
+	m_D3D9pp.BackBufferFormat = D3DFMT_A8R8G8B8;//后台缓冲像素格式
+	m_D3D9pp.BackBufferCount = 1;//后台缓冲数量(1)
+	m_D3D9pp.MultiSampleType = D3DMULTISAMPLE_NONE;//后台缓冲多重采样类型
+	m_D3D9pp.MultiSampleQuality = 0;//后台缓冲多重采样质量
+	m_D3D9pp.SwapEffect = D3DSWAPEFFECT_DISCARD;//交换链页面置换方式
+	m_D3D9pp.hDeviceWindow = hWnd;//设备相关窗口句柄
+	m_D3D9pp.Windowed = bIsWindowed;//窗口模式:true/全屏模式:false
+	m_D3D9pp.EnableAutoDepthStencil = true;//Direct3D自动创建维护深度缓冲和模板缓冲
+	m_D3D9pp.AutoDepthStencilFormat = D3DFMT_D24S8;//深度缓冲和模板缓冲像素格式
+	m_D3D9pp.Flags = 0;//无标记格式
+	m_D3D9pp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;//Direct3D默认刷新频率
+	m_D3D9pp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;//Direct3D提交频率(默认提交)
+
+	//创建IDirect3DDevice9接口对象指针
+	VERIFY(m_pD3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, Vp, &m_D3D9pp, &m_pD3D9Device));//创建IDirect3DDevice9接口对象指针
+
+	//读取GPU型号
+	m_pD3D9->GetAdapterIdentifier(0, 0, &D3D9Adapter);	//读取GPU信息
+	nSize = MultiByteToWideChar(CP_ACP, 0, D3D9Adapter.Description, -1, NULL, 0);
+	MultiByteToWideChar(CP_ACP, 0, D3D9Adapter.Description, -1, m_wcD3D9AdapterType, nSize);
+
+	return S_OK;//OK
+}
+
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsInit(D3DPRESENT_PARAMETERS D3D9pp)
+// @Purpose: DirectGraphics 初始化
+// @Since: v1.00a
+// @Para: D3DPRESENT_PARAMETERS D3D9pp(D3D9参数)
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsInit(D3DPRESENT_PARAMETERS D3D9pp)
+{
+	int nSize;
+	int Vp;
+	D3DADAPTER_IDENTIFIER9 D3D9Adapter;
+
+	//读取Direct3D9接口对象指针
+	m_pD3D9 = Direct3DCreate9(D3D_SDK_VERSION);//创建Direct3D9接口对象
+
+	//读取D3DCAPS9校验硬件顶点运算
+	VERIFY(m_pD3D9->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &m_D3D9Caps));//校验硬件顶点运算能力
+
+	if (m_D3D9Caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT)
+	{
+		Vp = D3DCREATE_HARDWARE_VERTEXPROCESSING;//支持硬件顶点运算(硬件顶点运算)
+	}
+	else
+	{
+		Vp = D3DCREATE_SOFTWARE_VERTEXPROCESSING;//不支持硬件顶点运算(软件顶点运算)
+	}
+
+	//填充D3DPRESENT_PARAMETERS结构
+	m_D3D9pp.BackBufferWidth = D3D9pp.BackBufferWidth;//后台缓冲表面宽度(Pixel)
+	m_D3D9pp.BackBufferHeight = D3D9pp.BackBufferHeight;//后台缓冲表面高度(Pixel)
+	m_D3D9pp.BackBufferFormat = D3D9pp.BackBufferFormat;//后台缓冲像素格式
+	m_D3D9pp.BackBufferCount = D3D9pp.BackBufferCount;//后台缓冲数量(1)
+	m_D3D9pp.MultiSampleType = D3D9pp.MultiSampleType;//后台缓冲多重采样类型
+	m_D3D9pp.MultiSampleQuality = D3D9pp.MultiSampleQuality;//后台缓冲多重采样质量
+	m_D3D9pp.SwapEffect = D3D9pp.SwapEffect;//交换链页面置换方式
+	m_D3D9pp.hDeviceWindow = D3D9pp.hDeviceWindow;//设备相关窗口句柄
+	m_D3D9pp.Windowed = D3D9pp.Windowed;//窗口模式:true/全屏模式:false
+	m_D3D9pp.EnableAutoDepthStencil = D3D9pp.EnableAutoDepthStencil;//Direct3D自动创建维护深度缓冲和模板缓冲
+	m_D3D9pp.AutoDepthStencilFormat = D3D9pp.AutoDepthStencilFormat;//深度缓冲和模板缓冲像素格式
+	m_D3D9pp.Flags = D3D9pp.Flags;//无标记格式
+	m_D3D9pp.FullScreen_RefreshRateInHz = D3D9pp.FullScreen_RefreshRateInHz;//Direct3D默认刷新频率
+	m_D3D9pp.PresentationInterval = D3D9pp.PresentationInterval;//Direct3D提交频率(默认提交)
+
+	//创建IDirect3DDevice9接口对象指针
+	VERIFY(m_pD3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, D3D9pp.hDeviceWindow, Vp, &m_D3D9pp, &m_pD3D9Device));//创建IDirect3DDevice9接口对象指针
+
+	//读取GPU型号
+	m_pD3D9->GetAdapterIdentifier(0, 0, &D3D9Adapter);	//读取GPU信息
+	nSize = MultiByteToWideChar(CP_ACP, 0, D3D9Adapter.Description, -1, NULL, 0);
+	MultiByteToWideChar(CP_ACP, 0, D3D9Adapter.Description, -1, m_wcD3D9AdapterType, nSize);
+
+	return S_OK;//OK
+}
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsBeginScene(void)
+// @Purpose: DirectGraphics 开始渲染
+// @Since: v1.00a
+// @Para: None
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsBeginScene(void)
+{
+	VERIFY(m_pD3D9Device->BeginScene());	//开始渲染
+	return S_OK;//OK
+}
+
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsEndScene(void)
+// @Purpose: DirectGraphics 结束渲染
+// @Since: v1.00a
+// @Para: None
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsEndScene(void)
+{
+	VERIFY(m_pD3D9Device->EndScene());		//结束渲染
+	return S_OK;//OK
+}
+
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsBegin(void)
+// @Purpose: DirectGraphics 开始渲染
+// @Since: v1.00a
+// @Para: None
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsBegin(void)
+{
+	VERIFY(m_pD3D9Device->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0));		//清空图像
+	VERIFY(m_pD3D9Device->BeginScene());	//开始渲染
+	return S_OK;//OK
+}
+
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsEnd(void)
+// @Purpose: DirectGraphics 结束渲染
+// @Since: v1.00a
+// @Para: None
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsEnd(void)
+{
+	VERIFY(m_pD3D9Device->EndScene());		//结束渲染
+	VERIFY(m_pD3D9Device->Present(NULL, NULL, NULL, NULL));		//提交渲染(显示)
+	return S_OK;//OK
+}
+
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsPresent(void)
+// @Purpose: DirectGraphics 提交渲染(显示)
+// @Since: v1.00a
+// @Para: None
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsPresent(void)
+{
+	VERIFY(m_pD3D9Device->Present(NULL,NULL,NULL,NULL));		//提交渲染(显示)
+	return S_OK;//OK
+}
+
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsClear(void)
+// @Purpose: DirectGraphics 清空图像
+// @Since: v1.00a
+// @Para: None
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsClear(void)
+{
+	VERIFY(m_pD3D9Device->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0));		//清空图像
+	return S_OK;//OK
+}
+
+//---------------------------------------------------------------------------------------------------
+// @Function:	 DirectGraphicsClear(DWORD dwColor)
+// @Purpose: DirectGraphics 清空图像
+// @Since: v1.00a
+// @Para: DWORD dwColor(背景颜色)
+// @Return: HRESULT(初始化状态:成功:S_OK,失败:E_FAIL)
+//---------------------------------------------------------------------------------------------------
+HRESULT WINAPI DirectGraphics::DirectGraphicsClear(DWORD dwColor)
+{
+	VERIFY(m_pD3D9Device->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, dwColor, 1.0f, 0));		//清空图像
+	return S_OK;//OK
+}
---
+++ DirectGraphics/DirectGraphics.h
@@ -0,0 +1,75 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017, Alopex/Helium
+*     All rights reserved.
+*
+* @file		DirectGraphics.h
+* @brief	This Program is DirectGraphics DLL Project.
+* @author	Alopex/Helium
+* @version	v1.00a
+* @date		2017-11-2
+*/
+#pragma once
+
+#ifndef __DIRECTGRAPHICS_H_
+#define __DIRECTGRAPHICS_H_
+
+//Macro Definition
+#ifdef	DIRECTGRAPHICS_EXPORTS
+#define DIRECTGRAPHICS_API	__declspec(dllexport)
+#else
+#define DIRECTGRAPHICS_API	__declspec(dllimport)
+#endif
+
+#ifndef USER_SCREENWIDTH
+#define USER_SCREENWIDTH	640		//UserScreenWidth
+#endif
+
+#ifndef USER_SCREENHEIGHT
+#define USER_SCREENHEIGHT	480		//UserScreenHeight
+#endif
+
+#define ADAPTERTYPESIZE	50	//AdapterType Size
+
+//Struct Definition
+
+//CallBack Definition
+
+//Class Definition
+class DIRECTGRAPHICS_API DirectGraphics
+{
+private:
+	IDirect3D9* m_pD3D9;	//The Direct3D 9 Main Object
+	IDirect3DDevice9* m_pD3D9Device;	//The Direct3D 9 Render Device
+	D3DCAPS9 m_D3D9Caps;	//The Direct3D 9 Caps
+	D3DPRESENT_PARAMETERS m_D3D9pp;		//The Direct3D 9 Parameters
+	wchar_t m_wcD3D9AdapterType[ADAPTERTYPESIZE];	//The Direct3D 9 AdapterType(eg:NVIDIA GeForce GT750M)
+
+public:
+	DirectGraphics();	//DirectGraphics Constructor Function
+	~DirectGraphics();	//DirectGraphics Destructor	Function
+
+	IDirect3D9* WINAPI DirectGraphicsGetObject(void) const;	//DirectGraphics Get Object
+	IDirect3DDevice9* WINAPI DirectGraphicsGetDevice(void) const;	//DirectGraphics Get Device
+	const D3DCAPS9* WINAPI DirectGraphicsGetCaps(void) const;	//DirectGraphics Get Caps
+	const D3DPRESENT_PARAMETERS* WINAPI DirectGraphicsGetPresentParameters(void) const;	//DirectGraphics Get Present Parameters
+	const wchar_t* WINAPI DirectGraphicsAdapterType(void) const;	//DirectGraphics Get Adapter Type
+
+	HRESULT WINAPI DirectGraphicsInit(HWND hWnd);	//DirectGraphics Initialization(W:640 H:480 Windowed)
+	HRESULT WINAPI DirectGraphicsInit(HWND hWnd, bool bIsWindowed);	//DirectGraphics Initialization
+	HRESULT WINAPI DirectGraphicsInit(HWND hWnd, bool bIsWindowed, int nScreenWidth, int nScreenHeight);	//DirectGraphics Initialization
+	HRESULT WINAPI DirectGraphicsInit(D3DPRESENT_PARAMETERS D3D9pp);	//DirectGraphics Initialization
+	HRESULT WINAPI DirectGraphicsBeginScene(void);	//DirectGraphics Begin Render
+	HRESULT WINAPI DirectGraphicsEndScene(void);	//DirectGraphics End Render
+	HRESULT WINAPI DirectGraphicsBegin(void);	//DirectGraphics Begin Render
+	HRESULT WINAPI DirectGraphicsEnd(void);		//DirectGraphics End Render
+	HRESULT WINAPI DirectGraphicsPresent(void);	//DirectGraphics Present Render
+	HRESULT WINAPI DirectGraphicsClear(void);	//DirectGraphics Clear Screen
+	HRESULT WINAPI DirectGraphicsClear(DWORD dwColor);	//DirectGraphics Clear Screen
+};
+
+//Variable Definition
+
+//Function Definition
+
+#endif
